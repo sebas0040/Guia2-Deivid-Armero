b7361720ffe3aa8261fe9e3c55622752b7737c0d
sebas0040 <deividsebastianarmero@gmail.com>
2024-09-13 13:20:21 -0500

    El proyecto de angular se encuentra en la direccion \Guia2-Deivid-Armero\guia2-Deivid-Armero. El html esta en app.components.html y el css esta en styles.css. Esta actividad se subio a la rama main

---
+++ guia2-Deivid-Armero/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'guia2-Deivid-Armero' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('guia2-Deivid-Armero');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, guia2-Deivid-Armero');
+  });
+});
